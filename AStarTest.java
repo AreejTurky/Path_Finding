43d587dc64fc8803d9c340d0baff1eb9487828f9
Areej AlOtaibi <72670290+AreejTurky@users.noreply.github.com>
2021-11-28 13:34:11 +0300

    Update AStarTest.java

--- AStarTest.java
+++ AStarTest.java
@@ -8,9 +8,9 @@ public class AStarTest {
         Node initalNode=  new Node(2, 1);      //initial node 
         Node [] goals = new Node[4];
         goals[0]= new Node(1, 6);       //subgoal node 
-        goals[1] = new Node(1, 3);       //Final node
-        goals[2] = new Node(4, 5);       //Final node
-        goals[3]= new Node(5, 6);
+        goals[1] = new Node(1, 3);       //subgoal node
+        goals[2] = new Node(4, 5);       //subgoal node
+        goals[3]= new Node(5, 6);       //Final node
 
         int rows = 6;   
         int cols = 7;
